71172164afea6376b6fa56df1e819aee2fabf001
Harshith-Marneni <75963605+Harshith-Marneni@users.noreply.github.com>
2020-12-14 16:29:21 -0500

    Add files via upload

---
+++ reversingastring.java
@@ -0,0 +1,42 @@
+package src;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class reversingastring {
+
+	
+	private static boolean False;
+
+	public static void main(String[] args) {
+		int i;
+		Scanner n = new Scanner(System.in);
+	    System.out.println("Enter a word : ");
+		String s = n.nextLine();
+	    Stack<Character> stacks = new Stack<Character>();
+		
+		for(i = 0; i < s.length(); i++)
+		{
+			stacks.push(s.charAt(i));
+		}
+		
+		System.out.println("Reverse of entered word : ");
+		
+        if(stacks.empty()==False)
+        {
+		while(stacks.empty()==False)
+		{
+			System.out.println(stacks.pop());
+		}
+		System.out.println("Reverse operation is performed.");
+        }
+
+		
+		if(stacks.empty())
+		{
+			System.out.println("Hence not performed.");
+		}
+		
+	}
+
+}
